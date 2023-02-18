d6957b9218dfcc292e6629d874e4eb3df930bda5
Ahsan Habib <ahnazir.ahn@gmail.com>
2023-02-18 10:00:15 +0600

    hello world print and php variables

---
+++ 002_PHP Fundamental 01/02-variable.php
@@ -0,0 +1,9 @@
+<?php
+    $name = "Ahsan Habib";  //String Type Variable
+    $age = 26;              // Integer Type Variable;
+    $experience = 2.5;      // Double Type Variable
+    
+    echo "My Name is ".$name.PHP_EOL."I'm "
+                      .$age." years old".PHP_EOL    
+                      ."I have ".$experience." Years of coding Experience".PHP_EOL; // String and Variable concatenation using dot (.) Operator and PHP_EOL is used to print newline
+?>
